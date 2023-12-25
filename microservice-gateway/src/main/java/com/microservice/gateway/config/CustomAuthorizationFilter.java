c1d69cdb5d6535470df67235a7cffafef4b17200
leonf <ldavis@unsa.edu.pe>
2023-12-24 23:31:12 -0500

    salioooooooooooo

--- microservice-gateway/src/main/java/com/microservice/gateway/config/CustomAuthorizationFilter.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/config/CustomAuthorizationFilter.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
+import org.springframework.web.server.ServerWebExchange;
+
 
 @Component
 public class CustomAuthorizationFilter extends AbstractGatewayFilterFactory<CustomAuthorizationFilter.Config> {
@@ -12,33 +13,27 @@ public class CustomAuthorizationFilter extends AbstractGatewayFilterFactory<Cust
     public CustomAuthorizationFilter() {
         super(Config.class);
     }
-
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            // Aquí puedes obtener el valor que deseas verificar, por ejemplo, desde un encabezado o un atributo de la solicitud
-            boolean shouldAllow = false;
-
-            if (shouldAllow) {
-                // Si el valor es true, permitir la solicitud
+            // Verifica la lógica de autorización aquí
+            if (isAuthorized(exchange, config)) {
                 return chain.filter(exchange);
             } else {
-                // Si el valor es false, bloquear la solicitud y devolver un código de estado 403 (Prohibido)
                 exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
                 return exchange.getResponse().setComplete();
             }
         };
     }
 
+    private boolean isAuthorized(ServerWebExchange exchange, Config config) {
+        // Implementa la lógica de autorización según tus necesidades
+        // Puedes verificar encabezados, tokens, roles, etc.
+        // En este ejemplo, simplemente permitimos todo (puedes cambiar esto según tus requisitos)
+        return false;
+    }
+
     public static class Config {
-        private boolean allow;
-
-        public boolean isAllow() {
-            return allow;
-        }
-
-        public void setAllow(boolean allow) {
-            this.allow = allow;
-        }
+        // Puedes agregar configuraciones específicas si es necesario
     }
 }
