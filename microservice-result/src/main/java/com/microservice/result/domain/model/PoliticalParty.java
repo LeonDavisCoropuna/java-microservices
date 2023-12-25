97b993b835a9d14de69a760e28a81f0bb07c9adb
lino62U <alupoc@unsa.edu.pe>
2023-12-25 11:33:03 -0500

    first commit

---
+++ microservice-result/src/main/java/com/microservice/result/domain/model/PoliticalParty.java
@@ -0,0 +1,18 @@
+package org.system.onlineelection.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//model domain
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class PoliticalParty {
+
+    private Integer id;
+    private String namePoliticalParty;
+    private Integer numVotes;
+}
---
+++ microservice-result/src/main/java/com/microservice/result/infrastructure/controller/ResultController.java
@@ -0,0 +1,2 @@
+package com.microservice.result.infrastructure.controller;public class ResultController {
+}
